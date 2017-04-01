a6652e42fdc4787cfe0e7a0e998180ba1f0a839f
Eric Zucho <ericzucho@gmail.com>
2017-03-31 19:18:16 -0500

    Ace editor installation. Currently uses JS syntax

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,11 @@
 // This is where it all goes :)
+//= require "jquery"
+//= require "vendor/ace/ace"
+//= require "vendor/ace/theme-monokai"
+//= require "vendor/ace/mode-javascript"
+
+$(function () {
+	var editor = ace.edit("editor");
+	editor.setTheme("ace/theme/monokai");
+	editor.getSession().setMode("ace/mode/javascript");
+});

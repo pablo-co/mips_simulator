e8523b4ab33f862f66423126b72fb14e0968b10c
Eric Zucho <ericzucho@gmail.com>
2017-04-07 16:33:13 -0500

    Detection of undeclared labels in branches and jumps

--- source/javascripts/mips_pipeline.js
+++ source/javascripts/mips_pipeline.js
@@ -1,11 +1,41 @@
 function callParser() {
   	var editor = ace.edit("editor");
-  	var result_area = $("#parse_result_area")[0];
+  	$("#parse_result_area")[0].value = "";
+  	var instruction_set;
   	$("#parse_button")[0].disabled = true;
   	try {
-  		  	result_area.value = parser_instance.parse(editor.getValue());
+  		instruction_set = parser_instance.parse(editor.getValue());
   	} catch(err) {
-  		result_area.value = err;
+  		printError(err);
+  		return;
   	}
+  	var nonexistentLabels = obtainNonexistentLabels(instruction_set);
+  	if (nonexistentLabels.length > 0) {
+  		printError("The following markers are used in branches or jumps without being defined: " + nonexistentLabels);
+  		return;
+  	}
+}
 
+function obtainNonexistentLabels(instruction_set) {
+	var nonexistentLabels = [];
+	var markersFound = [];
+	instruction_set.forEach(function(instruction) {
+		if (instruction.marker != null) {
+			markersFound.push(instruction.marker);
+		}
+
+	});
+	instruction_set.forEach(function(instruction) {
+		if (instruction.label != null) { 
+			if ($.inArray(instruction.label, markersFound) == -1) {
+				nonexistentLabels.push(instruction.label);
+			}
+		}
+	});
+	return nonexistentLabels;
+}
+
+function printError(error_string){
+	var error_output_area = $("#parse_result_area")[0];
+	error_output_area.value = error_string;
 }
